a8c1a9336cbcdd03abae49a5ef3ef9ff8c9fe126
Akash Biswas <akashrahul2006@gmail.com>
2025-08-08 13:22:07 +0530

    vercel path rewrite added

--- Server/Auth/Src/Model/auth.model.js
+++ Server/Auth/Src/Model/auth.model.js
@@ -5,7 +5,6 @@ const AuthSchema = new Schema(
     name: { type: String, required: true },
     password: { type: String, default: "" },
     email: { type: String, required: true, unique: true },
-    avatar: { type: String, default: "" },
     userId: { type: String, default: "" }, //! pass via rabitmq
     isVerified: { type: Boolean, default: false },
     lastLogin: { type: Date, default: Date.now },
